3b96d876b1bf4ad69e95af9d42be6dd6b8daa4d0
vijayarun_ <64945040+vijayarun00100@users.noreply.github.com>
2024-02-11 20:39:57 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,21 @@
+/*// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAz8Q7z3yVMUizHo0lDSWM_XPvCKArMMic",
+  authDomain: "reva-edu.firebaseapp.com",
+  projectId: "reva-edu",
+  storageBucket: "reva-edu.appspot.com",
+  messagingSenderId: "446136969099",
+  appId: "1:446136969099:web:b7ac2970147d9ad7b2718e",
+  measurementId: "G-BSJ9RC0NBW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);*/
